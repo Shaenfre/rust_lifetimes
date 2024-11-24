e4a5971d0739ae030007985ee1ee61aee9fcc0c4
Shaenfre <rajs93222@gmail.com>
2024-11-24 23:05:16 +0530

    implemented next for iterator

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,17 @@ impl StrSplit {
 impl Iterator for StrSplit {
     type Item = &str
     fn next(&mut self) -> Option<Self::Item> {
-
+        if let Some(next_delim) = self.remainder.find(self.delim) {
+            let until_delim = &self.remainder[::next_delim];
+            self.remainder = &self.remainder[(next_delim + len(delim))::];
+            Some(until_delim)
+        } else if self.remainder.is_empty() {
+            None
+        } else {
+            let rest = self.remainder;
+            self.remainder = &[];
+            Some(rest)
+        }
     }
 }
 
