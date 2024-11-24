e420c31af6bb7b50a393842a17085807473d9005
Shaenfre <rajs93222@gmail.com>
2024-11-24 22:47:01 +0530

    initial implementation and test

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,28 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+struct StrSplit {
+    remainder: &str,
+    delim: &str
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl StrSplit {
+    fn new(input_str: &str, delim: &str) -> Self {
+        Self {
+            remainder: input_str,
+            delim,
+        }
     }
+}
+
+impl Iterator for StrSplit {
+    type Item = &str
+    fn next(&mut self) -> Option<Self::Item> {
+
+    }
+}
+
+#[test]
+fn it_works() {
+    let input_str = "a b c d e";
+    let letters = StrSplit::new(input_str, " ").collect;
+    asserteq!(input_str, letters);
+
 }
