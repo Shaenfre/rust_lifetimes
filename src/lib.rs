fe71570489ed885b38b715feac2988a8eb68d998
Shaenfre <66408159+Shaenfre@users.noreply.github.com>
2024-11-26 16:50:26 +0530

    first working version

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-struct StrSplit {
-    remainder: &str,
-    delim: &str
+#[derive(Debug)]
+struct StrSplit<'a> {
+    remainder: &'a str,
+    delim: &'a str,
 }
 
-impl StrSplit {
-    fn new(input_str: &str, delim: &str) -> Self {
+impl<'a> StrSplit<'a> {
+    fn new(input_str: &'a str, delim: &'a str) -> Self {
         Self {
             remainder: input_str,
             delim,
@@ -12,18 +13,18 @@ impl StrSplit {
     }
 }
 
-impl Iterator for StrSplit {
-    type Item = &str
+impl<'a> Iterator for StrSplit<'a> {
+    type Item = &'a str;
     fn next(&mut self) -> Option<Self::Item> {
         if let Some(next_delim) = self.remainder.find(self.delim) {
-            let until_delim = &self.remainder[::next_delim];
-            self.remainder = &self.remainder[(next_delim + len(delim))::];
+            let until_delim = &self.remainder[..next_delim];
+            self.remainder = &self.remainder[(next_delim + self.delim.len())..];
             Some(until_delim)
         } else if self.remainder.is_empty() {
             None
         } else {
             let rest = self.remainder;
-            self.remainder = &[];
+            self.remainder = "";
             Some(rest)
         }
     }
@@ -32,7 +33,17 @@ impl Iterator for StrSplit {
 #[test]
 fn it_works() {
     let input_str = "a b c d e";
-    let letters = StrSplit::new(input_str, " ").collect;
-    asserteq!(input_str, letters);
+    let letters: Vec<_> = StrSplit::new(input_str, " ").collect();
+    // let expected: Vec<&str> = vec!["a", "b", "c", "d", "e"];
+    // assert_eq!(letters.collect::<Vec<_>>(), expected);
+    assert_eq!(letters, vec!["a", "b", "c", "d", "e"])
+}
 
+#[test]
+fn tail() {
+    let input_str = "a b c d ";
+    let letters: Vec<_> = StrSplit::new(input_str, " ").collect();
+    // let expected: Vec<&str> = vec!["a", "b", "c", "d", "e"];
+    // assert_eq!(letters.collect::<Vec<_>>(), expected);
+    assert_eq!(letters, vec!["a", "b", "c", "d", " "])
 }
